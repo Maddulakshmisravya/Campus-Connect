72d9ad40075a75a6d0196eb4ffa2cd87eea9ac44
sravya <sravya>
2026-04-01 19:45:58 +0530

    check deployment update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,8 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
+console.log("API URL:", "https://campus-connect-rype.onrender.com");
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
